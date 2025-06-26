2b534e8ff4cfbf3290084c453d682932ce623a7f
alexshenker <alekseyshenker@gmail.com>
2025-06-26 14:23:10 -0400

    known page setup

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,9 @@
+const Admin = (): React.JSX.Element => {
+    return (
+        <div>
+            <div></div>
+        </div>
+    );
+};
+
+export default Admin;
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+const Dashboard = (): React.JSX.Element => {
+    return (
+        <div>
+            <div></div>
+        </div>
+    );
+};
+
+export default Dashboard;
